e09456cbf9afb47a8a93de567fa6f755c2eaf6f6
Mathieu Gerard <mathieu2991@gmail.com>
2024-05-19 01:27:38 +0200

    fix bug page success

--- src/views/Premium/Success.js
+++ src/views/Premium/Success.js
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
-import { getFormatDatePremium, getCookie } from '../../api/APIutils';
-import { useDispatch} from 'react-redux';
+import { getFormatDatePremium } from '../../api/APIutils';
+import { fetchData, getCookie } from '../../api/APIutils';
 import { loadToken, loadUserMe } from '../../features/user/userSlice';
+import { useDispatch} from 'react-redux';
 
 export default function Success(){
     const dispatch = useDispatch();
@@ -19,53 +20,44 @@ export default function Success(){
             const body = {
                 refresh_token: cookie
             }
-            const bodyJson = JSON.stringify(body);
-            fetch(`${process.env.REACT_APP_URL}api/token/refresh`, 
-                {
-                    method: "POST",
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: bodyJson
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        console.log(response)
-                    }    
-                    return response.json()
-                })
-                .then(data => {
-                    if (data.token) {
-                        dispatch(loadToken(data.token));
-                        const userJson = JSON.parse(data.user);
-                        dispatch(loadUserMe(userJson));
-                    }
-                })
+            fetchData('token/refresh', 'POST', loadData, '', body)
         }
     }, [])
 
+    function loadData(data) {
+        if (data.token) {
+            dispatch(loadToken(data.token));
+            const userJson = JSON.parse(data.user);
+            dispatch(loadUserMe(userJson));
+        }
+    }
+
     return (
         <div className='d-flex flex-column'>
-            <div className='d-flex justify-content-center'>
-                <>{isExploding && <ConfettiExplosion />}</>
-                <h2>
-                    Félicitations pour votre abonnement { user.username } !
-                </h2>
-            </div>
             {
-                !user.subscriptions ?
-                    <div>
-                        <p>
-                            Votre abonnement est actuellement en cours de validation, patientez un petit moment, ça ne devrait plus tarder.
-                        </p>
+                user.subscriptions &&
+                <>
+                    <div className='d-flex justify-content-center'>
+                        <>{isExploding && <ConfettiExplosion />}</>
+                        <h2>
+                            Félicitations pour votre abonnement { user.username } !
+                        </h2>
                     </div>
-                    :
-                    <div className='mt-4'>
-                        <p>
-                            Votre abonnement se renouvellera automatiquement le { getFormatDatePremium(user.subscriptions[0].premiumExpiredAt)}
-                        </p>
-                    </div>
+                    {
+                    user.subscriptions.length === 0 ?
+                        <div className='mt-4'>
+                            <p>
+                                Votre abonnement est actuellement en cours de validation, patientez un petit moment, ça ne devrait plus tarder.
+                            </p>
+                        </div>
+                        :
+                        <div className='mt-4'>
+                            <p>
+                                Votre abonnement se renouvellera automatiquement le { getFormatDatePremium(user.subscriptions[0].premiumExpiredAt)}
+                            </p>
+                        </div>
+                    }
+                </>
             }
         </div>
     )
