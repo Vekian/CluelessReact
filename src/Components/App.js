f5e93649792a4637e08ec53430c96f275abdd6a1
Vekian <mathieu2991@gmail.com>
2025-08-29 18:00:35 +0200

    modif url api

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,36 +1,39 @@
-import './App.css';
-import Header from './Header/Header';
-import Sidemenu from './Sidemenu/Sidemenu';
-import SubMenu from './Submenu/Submenu';
-import Main from './Main';
-import React, { useContext } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { UIContext } from './UIProvider';
-import Login from '../Components/Header/Login/Login';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Admin from '../views/Admin/Admin';
+import "./App.css";
+import Header from "./Header/Header";
+import Sidemenu from "./Sidemenu/Sidemenu";
+import SubMenu from "./Submenu/Submenu";
+import Main from "./Main";
+import React, { useContext } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { UIContext } from "./UIProvider";
+import Login from "../Components/Header/Login/Login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Admin from "../views/Admin/Admin";
 
 export default function App() {
-  const {darkMode, clueMode} = useContext(UIContext);
-        
+  const { darkMode, clueMode } = useContext(UIContext);
 
   return (
-    <div className={`App vh-100 ${darkMode ? 'dark' : 'light'} ${clueMode && "clue"}`}>
+    <div
+      className={`App vh-100 ${darkMode ? "dark" : "light"} ${
+        clueMode && "clue"
+      }`}
+    >
       <GoogleOAuthProvider clientId="1047880689996-3jmsj1nv0ekn34ur1h2r1rjonbbvgiuf.apps.googleusercontent.com">
-      <Router basename="/admin">
-        <Admin />
-      </Router>
-      < Router basename='/app'>
-        < Header />
-        < SubMenu />
-        <div className='d-flex bodySection'>
-          < Sidemenu />
-          <div className='d-flex flex-column col-sm-9 col-lg-10 col-12 mainContener'>
-              < Main />
+        <Router basename="/admin">
+          <Admin />
+        </Router>
+        <Router basename="/app">
+          <Header />
+          <SubMenu />
+          <div className="d-flex bodySection">
+            <Sidemenu />
+            <div className="d-flex flex-column col-sm-9 col-lg-10 col-12 mainContener">
+              <Main />
+            </div>
           </div>
-        </div>
-        <Login />
-      </ Router >
+          <Login />
+        </Router>
       </GoogleOAuthProvider>
     </div>
   );
